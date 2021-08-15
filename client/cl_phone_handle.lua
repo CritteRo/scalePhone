84fc7d13cba7df503442e1acb57c68e9b9325285
CritteRo <critteras@gmail.com>
2021-08-15 15:42:07 +0300

    cleanup :clap:

--- client/cl_phone_handle.lua
+++ client/cl_phone_handle.lua
@@ -25,63 +25,6 @@ apps = {
     buttons = {
         }
     },
-    --[[ CUSTOM APPS ]]--
-    --[[[1] = {appID = 1, id = 2, isLeftToRight = false, type = "contacts", name = "Contacts", icon = 5, notif = 0, openEvent = "scalePhone.OpenContacts", backEvent = "scalePhone.GoToHomepage", data = {backApp = 0},
-    buttons = {
-            --[0] = {name = "Contact", pic = 'CHAR_BLANK_ENTRY', isBot = false, event = ""},
-        }
-    },
-    [2] = {appID = 2,id = 6, isLeftToRight = false, type = "messagesList", name = "Messages", icon = 2, notif = 0, openEvent = "scalePhone.OpenMessages", backEvent = "scalePhone.GoToHomepage", data = {backApp = 0},
-    buttons = {
-            --[0] = {contact = "Lili", h = 15, m = 10, message = "Nelu tea rugat iulia sa idai si ei osuta dojda mi sa sia tigari si maine ti da", event = "scalePhone.ShowMessage", isentthat = false},
-        }
-    },
-    [3] = {appID = 3,id = 8, isLeftToRight = false, type = "emailList", name = "Emails", icon = 4, notif = 0, openEvent = "scalePhone.OpenEmails", backEvent = "scalePhone.GoToHomepage", data = {backApp = 0},
-    buttons = {
-            --[0] = {title = "Update 1", to = "fivem.net", from = "homies.net", message = "test"},
-        }
-    },
-    [4] = {appID = 4,id = 0, isLeftToRight = false, type = "snapmatic", name = "Snapmatic", icon = 1, notif = 0, openEvent = "scalePhone.OpenSnapmatic", backEvent = "scalePhone.GoToHomepage", data = {backApp = 0},
-        buttons = {
-            [0] = {"blow_kiss"},
-            [1] = {"dock"},
-            [2] = {"jazz_hands"},
-            [3] = {"the_bird"},
-            [4] = {"thumbs_up"},
-            [5] = {"wank"},
-        }
-    },
-    [5] = {appID = 5,id = 14, isLeftToRight = false, type = "todoList", name = "Stats", icon = 12, notif = 0, openEvent = "scalePhone.OpenStatsMenu", backEvent = "scalePhone.GoToHomepage", data = {backApp = 0},
-        buttons = {
-            [0] = {title = "UID", text = "14", procent = 0},
-            [1] = {title = "Rank", text = "4", procent = 0},
-            [2] = {title = "Level", text = "100", procent = 0},
-            [3] = {title = "Job", text = "Hitman", procent = 0},
-            [4] = {title = "Faction", text = "FIB", procent = 0},
-            [5] = {title = "Heist Points", text = "52", procent = 0},
-        }
-    },
-    [6] = {appID = 6,id = 18, isLeftToRight = false, type = "menu", name = "Themes", icon = 24, notif = 0, openEvent = "scalePhone.OpenCustomMenu", backEvent = "scalePhone.GoToHomepage", data = {backApp = 0},
-        buttons = {
-            [0] = {text = "Blue", event = "scalePhone.ChangePhoneTheme", eventParams = {themeID = 0}},
-            [1] = {text = "Green", event = "scalePhone.ChangePhoneTheme", eventParams = {themeID = 1}},
-            [2] = {text = "Red", event = "scalePhone.ChangePhoneTheme", eventParams = {themeID = 2}},
-            [3] = {text = "Orange", event = "scalePhone.ChangePhoneTheme", eventParams = {themeID = 3}},
-            [4] = {text = "Gray", event = "scalePhone.ChangePhoneTheme", eventParams = {themeID = 4}},
-            [5] = {text = "Purple", event = "scalePhone.ChangePhoneTheme", eventParams = {themeID = 5}},
-            [6] = {text = "Pink", event = "scalePhone.ChangePhoneTheme", eventParams = {themeID = 6}},
-        }
-    },
-    [7] = {appID = 7,id = 18, isLeftToRight = false, type = "menu", name = "eJobs", icon = 35, notif = 0, openEvent = "scalePhone.OpenCustomMenu", backEvent = "scalePhone.GoToHomepage", data = {backApp = 0},
-        buttons = {
-            [0] = {text = "Unemployed", event = "phoneJobs.ChangeJob", eventParams = {jobid = 0}}, 
-            [1] = {text = "Trucker", event = "phoneJobs.ChangeJob", eventParams = {jobid = 1}},
-            [2] = {text = "Farmer", event = "phoneJobs.ChangeJob", eventParams = {jobid = 2}},
-            [3] = {text = "Fisherman", event = "phoneJobs.ChangeJob", eventParams = {jobid = 3}},
-            [4] = {text = "Pilot", event = "phoneJobs.ChangeJob", eventParams = {jobid = 4}},
-            [5] = {text = "Hitman", event = "phoneJobs.ChangeJob", eventParams = {jobid = 5}},
-        }
-    },]]
 }
 
 RegisterCommand('phone', function()
